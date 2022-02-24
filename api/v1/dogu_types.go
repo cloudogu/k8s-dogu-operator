2da689cbeba612f44b98acb8c1ebf027ecf4f5f9
Joshua Sprey <joshua.sprey@cloudogu.com>
2022-02-24 11:19:49 +0100

    Add generated controller and dogu resource created by the kubebuilder for our controller

---
+++ api/v1/dogu_types.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DoguSpec defines the desired state of Dogu
+type DoguSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Name of the dogu (e.g. official/ldap)
+	Name string `json:"name,omitempty"`
+	// Version of the dogu (e.g. 2.4.48-3)
+	Version string `json:"version,omitempty"`
+}
+
+// DoguStatus defines the observed state of Dogu
+type DoguStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Dogu is the Schema for the dogus API
+type Dogu struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DoguSpec   `json:"spec,omitempty"`
+	Status DoguStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DoguList contains a list of Dogu
+type DoguList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Dogu `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Dogu{}, &DoguList{})
+}
