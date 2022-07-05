b52db93b3485b0a05f74fe703dc1cc98371fef51
Joshua Sprey <joshua.sprey@cloudogu.com>
2022-07-05 12:43:09 +0200

    #28 Fix annoying int test suite

--- controllers/suite_int_test.go
+++ controllers/suite_int_test.go
@@ -13,6 +13,8 @@ import (
         "github.com/bombsimon/logrusr/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+        "github.com/cloudogu/k8s-dogu-operator/controllers/limit"
+        mocks2 "github.com/cloudogu/k8s-dogu-operator/controllers/resource/mocks"
 
 	"github.com/cloudogu/cesapp-lib/core"
 	cesmocks "github.com/cloudogu/cesapp-lib/registry/mocks"
@@ -96,7 +98,10 @@ var _ = BeforeSuite(func() {
 	CesRegistryMock.On("DoguConfig", mock.Anything).Return(doguConfigurationContext)
 	CesRegistryMock.On("GlobalConfig").Return(globalConfigurationContext)
 
-	resourceGenerator := resource.NewResourceGenerator(k8sManager.GetScheme(), CesRegistryMock)
+	limitPatcher := &mocks2.LimitPatcher{}
+	limitPatcher.On("RetrieveMemoryLimits", mock.Anything).Return(limit.DoguLimits{}, nil)
+	limitPatcher.On("PatchDeployment", mock.Anything, mock.Anything).Return(nil)
+	resourceGenerator := resource.NewResourceGenerator(k8sManager.GetScheme(), limitPatcher)
 
 	version, err := core.ParseVersion("0.0.0")
 	Expect(err).ToNot(HaveOccurred())
