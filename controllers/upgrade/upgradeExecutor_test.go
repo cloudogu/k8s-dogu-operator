f69142e3d65ab2a8209cd2dce24528d260fcba3d
Philipp Pixel <boris.schaa@cloudogu.com>
2022-09-13 14:34:43 +0200

    #40 Add test

--- controllers/upgrade/upgradeExecutor_test.go
+++ controllers/upgrade/upgradeExecutor_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cloudogu/cesapp-lib/registry/mocks"
 	"github.com/cloudogu/k8s-dogu-operator/controllers/cesregistry"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -36,6 +37,28 @@ func Test_registerUpgradedDoguVersion(t *testing.T) {
 
 		// then
 		require.NoError(t, err)
+		registryMock.AssertExpectations(t)
+		doguRegistryMock.AssertExpectations(t)
+	})
+	t.Run("should fail", func(t *testing.T) {
+		toDoguCr := readTestDataRedmineCr(t)
+		toDoguCr.Spec.Version = "4.2.3-11"
+
+		toDogu := readTestDataDogu(t, redmineBytes)
+		toDogu.Version = "4.2.3-11"
+		doguRegistryMock := new(mocks.DoguRegistry)
+		registryMock := new(mocks.Registry)
+		registryMock.On("DoguRegistry").Return(doguRegistryMock)
+		doguRegistryMock.On("IsEnabled", toDogu.GetSimpleName()).Return(false, nil)
+
+		cesreg := cesregistry.NewCESDoguRegistrator(nil, registryMock, nil)
+
+		// when
+		err := registerUpgradedDoguVersion(cesreg, toDogu)
+
+		// then
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to register upgrade: could not register dogu version: previous version not found")
 		registryMock.AssertExpectations(t)
 		doguRegistryMock.AssertExpectations(t)
 	})
