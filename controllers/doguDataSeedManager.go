eb94cb7ec0e6b251cc324748a270499da5d8c392
Niklas <niklas.hinze@cloudogu.com>
2025-05-14 10:17:26 +0200

    #240 Refactor seed manager and add unit tests

--- controllers/doguDataSeedManager.go
+++ controllers/doguDataSeedManager.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
 	"github.com/cloudogu/retry-lib/retry"
-	v1 "k8s.io/api/core/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"reflect"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -20,14 +21,14 @@ const (
 )
 
 type doguDataSeedManager struct {
-	client              client.Client
+	deploymentInterface deploymentInterface
 	resourceGenerator   dataSeederInitContainerGenerator
 	resourceDoguFetcher resourceDoguFetcher
 }
 
-func NewDoguDataSeedManager(client client.Client, resourceGenerator dataSeederInitContainerGenerator, resourceDoguFetcher resourceDoguFetcher) *doguDataSeedManager {
+func NewDoguDataSeedManager(deploymentInterface deploymentInterface, resourceGenerator dataSeederInitContainerGenerator, resourceDoguFetcher resourceDoguFetcher) *doguDataSeedManager {
 	return &doguDataSeedManager{
-		client:              client,
+		deploymentInterface: deploymentInterface,
 		resourceGenerator:   resourceGenerator,
 		resourceDoguFetcher: resourceDoguFetcher,
 	}
@@ -36,13 +37,13 @@ func NewDoguDataSeedManager(client client.Client, resourceGenerator dataSeederIn
 func (m *doguDataSeedManager) DataMountsChanged(ctx context.Context, doguResource *v2.Dogu) (bool, error) {
 	logger := log.FromContext(ctx)
 	logger.Info(fmt.Sprintf("Determine if data mounts changed from dogu resource %s", doguResource.Name))
-	deployment, err := doguResource.GetDeployment(ctx, m.client)
+	deployment, err := m.getDoguDeployment(ctx, doguResource)
 	if err != nil {
 		return false, err
 	}
 
 	initContainers := deployment.Spec.Template.Spec.InitContainers
-	var actualDoguDataSeedContainer *v1.Container
+	var actualDoguDataSeedContainer *corev1.Container
 
 	// find init container
 	for _, container := range initContainers {
@@ -67,7 +68,19 @@ func (m *doguDataSeedManager) DataMountsChanged(ctx context.Context, doguResourc
 	return !reflect.DeepEqual(container, actualDoguDataSeedContainer), nil
 }
 
-func (m *doguDataSeedManager) createDataMountInitContainer(ctx context.Context, doguResource *v2.Dogu) (*v1.Container, error) {
+func (m *doguDataSeedManager) getDoguDeployment(ctx context.Context, doguResource *v2.Dogu) (*appsv1.Deployment, error) {
+	list, err := m.deploymentInterface.List(ctx, v1.ListOptions{LabelSelector: fmt.Sprintf("%s=%s", v2.DoguLabelName, doguResource.GetObjectKey().Name)})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get deployment for dogu %s: %w", doguResource.Name, err)
+	}
+	if len(list.Items) == 1 {
+		return &list.Items[0], nil
+	}
+
+	return nil, fmt.Errorf("dogu %s has more than one or zero deployments", doguResource.GetObjectKey().Name)
+}
+
+func (m *doguDataSeedManager) createDataMountInitContainer(ctx context.Context, doguResource *v2.Dogu) (*corev1.Container, error) {
 	dogu, _, err := m.resourceDoguFetcher.FetchWithResource(ctx, doguResource)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get dogu descriptor for dogu %s: %w", doguResource.Name, err)
@@ -89,14 +102,23 @@ func (m *doguDataSeedManager) UpdateDataMounts(ctx context.Context, doguResource
 	}
 
 	err = retry.OnConflict(func() error {
-		deployment, retryErr := doguResource.GetDeployment(ctx, m.client)
+		deployment, retryErr := m.getDoguDeployment(ctx, doguResource)
 		if retryErr != nil {
 			return retryErr
 		}
 
-		deployment.Spec.Template.Spec.InitContainers = append(deployment.Spec.Template.Spec.InitContainers, *container)
+		var updatedInitContainers []corev1.Container
+		for _, c := range deployment.Spec.Template.Spec.InitContainers {
+			if c.Name != dataSeedInitContainerName {
+				updatedInitContainers = append(updatedInitContainers, c)
+			}
+		}
+		updatedInitContainers = append(updatedInitContainers, *container)
+		deployment.Spec.Template.Spec.InitContainers = updatedInitContainers
 
-		return m.client.Update(ctx, deployment)
+		_, retryErr = m.deploymentInterface.Update(ctx, deployment, v1.UpdateOptions{})
+
+		return retryErr
 	})
 
 	if err != nil {
