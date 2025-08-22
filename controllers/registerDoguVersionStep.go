5d14cd48a347b97dcee437e1ae5daa08b7950244
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-08-22 06:51:14 +0200

    #259 fix register dogu

--- controllers/registerDoguVersionStep.go
+++ controllers/registerDoguVersionStep.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
@@ -10,15 +11,27 @@ import (
 
 type RegisterDoguVersionStep struct {
 	resourceDoguFetcher resourceDoguFetcher
+	doguRegistrator     doguRegistrator
+	localDoguFetcher    localDoguFetcher
 }
 
 func NewRegisterDoguVersionStep(mgrSet *util.ManagerSet) *RegisterDoguVersionStep {
 	return &RegisterDoguVersionStep{
 		resourceDoguFetcher: mgrSet.ResourceDoguFetcher,
+		localDoguFetcher:    mgrSet.LocalDoguFetcher,
+		doguRegistrator:     mgrSet.DoguRegistrator,
 	}
 }
 
 func (rdvs *RegisterDoguVersionStep) Run(ctx context.Context, doguResource *v2.Dogu) (requeueAfter time.Duration, err error) {
-	_, _, err = rdvs.resourceDoguFetcher.FetchWithResource(ctx, doguResource)
+	dogu, _, err := rdvs.resourceDoguFetcher.FetchWithResource(ctx, doguResource)
+	if err != nil {
+		return 0, fmt.Errorf("failed to fetch dogu descriptor: %w", err)
+	}
+	// TODO Can this function be used here? It should be used for new installations of a dogu.
+	err = rdvs.doguRegistrator.RegisterNewDogu(ctx, doguResource, dogu)
+	if err != nil {
+		return 0, fmt.Errorf("failed to register dogu: %w", err)
+	}
 	return 0, err
 }
