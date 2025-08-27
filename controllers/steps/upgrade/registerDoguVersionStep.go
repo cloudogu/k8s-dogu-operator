391dcec639e304db1394b9082c25a1c5e73046ea
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-08-27 09:05:43 +0200

    #259 check if version is already registered

--- controllers/steps/upgrade/registerDoguVersionStep.go
+++ controllers/steps/upgrade/registerDoguVersionStep.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	cloudoguerrors "github.com/cloudogu/ces-commons-lib/errors"
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/util"
@@ -29,6 +30,9 @@ func (rdvs *RegisterDoguVersionStep) Run(ctx context.Context, doguResource *v2.D
 
 	err = rdvs.doguRegistrator.RegisterDoguVersion(ctx, dogu)
 	if err != nil {
+		if cloudoguerrors.IsAlreadyExistsError(err) {
+			return steps.Continue()
+		}
 		return steps.RequeueWithError(fmt.Errorf("failed to register dogu: %w", err))
 	}
 
