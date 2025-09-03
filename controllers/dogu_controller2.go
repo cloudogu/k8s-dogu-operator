f9c9a607ae203e5ed6c7553096fba3752dea9bed
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-09-03 14:24:14 +0200

    #259 set ready condition

--- controllers/dogu_controller2.go
+++ controllers/dogu_controller2.go
@@ -9,23 +9,29 @@ import (
 	doguClient "github.com/cloudogu/k8s-dogu-lib/v2/client"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/config"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/health"
-	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/logging"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/usecase"
-	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	coreV1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+const (
+	ReasonReconcileSuccess = "ReconcileSuccess"
+	ReasonReconcileFail    = "ReconcileFail"
+	ReasonHasToReconcile   = "HasToReconcile"
 )
 
 type doguReconciler2 struct {
 	client            client.Client
 	doguChangeHandler DoguUsecase
 	doguDeleteHandler DoguUsecase
+	doguInterface     doguInterface
 }
 
 func NewDoguReconciler2(client client.Client, ecosystemClient doguClient.EcoSystemV2Interface, operatorConfig *config.OperatorConfig, eventRecorder record.EventRecorder, doguHealthStatusUpdater health.DoguHealthStatusUpdater, availabilityChecker *health.AvailabilityChecker) (*doguReconciler2, error) {
@@ -54,6 +60,7 @@ func NewDoguReconciler2(client client.Client, ecosystemClient doguClient.EcoSyst
 		client:            client,
 		doguChangeHandler: usecase.NewDoguInstallOrChangeUseCase(client, mgrSet, configRepos, eventRecorder, operatorConfig.Namespace, doguHealthStatusUpdater, doguRestartMgr),
 		doguDeleteHandler: usecase.NewDoguDeleteUsecase(client, mgrSet, configRepos, operatorConfig),
+		doguInterface:     ecosystemClient.Dogus(operatorConfig.Namespace),
 	}, nil
 }
 
@@ -63,15 +70,30 @@ func (r *doguReconciler2) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	doguResource := &doguv2.Dogu{}
 	err := r.client.Get(ctx, req.NamespacedName, doguResource)
 	if err != nil {
-		logger.Info(fmt.Sprintf("failed to get doguResource: %s", err))
+		logger.Error(err, fmt.Sprintf("failed to get doguResource: %s", err))
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+
 	var requeueAfter time.Duration
 	if doguResource.GetDeletionTimestamp().IsZero() {
 		requeueAfter, err = r.doguChangeHandler.HandleUntilApplied(ctx, doguResource)
 	} else {
 		requeueAfter, err = r.doguDeleteHandler.HandleUntilApplied(ctx, doguResource)
 	}
+
+	if err2 := r.client.Get(ctx, req.NamespacedName, doguResource); err2 != nil {
+		logger.Error(err, fmt.Sprintf("failed to get doguResource: %s", err2))
+		return ctrl.Result{}, err
+	}
+
+	if requeueAfter != 0 {
+		err = r.setReadyCondition(ctx, doguResource, metav1.ConditionFalse, ReasonHasToReconcile, fmt.Sprintf("The dogu resource has to be requeued after %d seconds.", requeueAfter))
+	} else if err != nil {
+		err = r.setReadyCondition(ctx, doguResource, metav1.ConditionFalse, ReasonReconcileFail, fmt.Sprintf("The dogu resource has to be requeued because of an error: %q.", err))
+	} else {
+		err = r.setReadyCondition(ctx, doguResource, metav1.ConditionTrue, ReasonReconcileSuccess, "The dogu resource has been reconciled successfully and is ready.")
+	}
+
 	return ctrl.Result{RequeueAfter: requeueAfter}, err
 }
 
@@ -87,4 +109,27 @@ func (r *doguReconciler2) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&coreV1.ServiceAccount{}).
 		Owns(&netv1.NetworkPolicy{}).
 		Complete(r)
+}
+
+func getReadyCondition(doguResource *doguv2.Dogu) *metav1.Condition {
+	return meta.FindStatusCondition(doguResource.Status.Conditions, doguv2.ConditionReady)
+}
+
+func (r *doguReconciler2) setReadyCondition(ctx context.Context, doguResource *doguv2.Dogu, status metav1.ConditionStatus, reason, message string) error {
+	logger := log.FromContext(ctx)
+	condition := metav1.Condition{
+		Type:               doguv2.ConditionReady,
+		Status:             status,
+		Reason:             reason,
+		Message:            message,
+		LastTransitionTime: metav1.Now(),
+	}
+	meta.SetStatusCondition(&doguResource.Status.Conditions, condition)
+	_, err := r.doguInterface.UpdateStatus(ctx, doguResource, metav1.UpdateOptions{})
+	if err != nil {
+		logger.Error(err, fmt.Sprintf("Failed to update dogu resource"))
+		return err
+	}
+	logger.Info(fmt.Sprintf("Updated dogu resource successfully!"))
+	return nil
 }
