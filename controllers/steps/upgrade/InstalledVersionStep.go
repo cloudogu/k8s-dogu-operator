09b6731ec9426b6e0f1ea481c2183c2ee03b9659
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-09-08 13:16:41 +0200

    #259 set installed version in Status

---
+++ controllers/steps/upgrade/InstalledVersionStep.go
@@ -0,0 +1,33 @@
+package upgrade
+
+import (
+	"context"
+
+	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
+	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps"
+	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/util"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type InstalledVersionStep struct {
+	doguInterface doguInterface
+}
+
+func NewInstalledVersionStep(mgrSet *util.ManagerSet, namespace string) *InstalledVersionStep {
+	return &InstalledVersionStep{
+		doguInterface: mgrSet.EcosystemClient.Dogus(namespace),
+	}
+}
+
+func (ivs *InstalledVersionStep) Run(ctx context.Context, doguResource *v2.Dogu) steps.StepResult {
+	doguResource, err := ivs.doguInterface.Get(ctx, doguResource.Name, v1.GetOptions{})
+	if err != nil {
+		steps.RequeueWithError(err)
+	}
+	doguResource.Status.InstalledVersion = doguResource.Spec.Version
+	doguResource, err = ivs.doguInterface.UpdateStatus(ctx, doguResource, v1.UpdateOptions{})
+	if err != nil {
+		return steps.RequeueWithError(err)
+	}
+	return steps.Continue()
+}
--- controllers/usecase/install_or_change_dogu.go
+++ controllers/usecase/install_or_change_dogu.go
@@ -54,6 +54,7 @@ func NewDoguInstallOrChangeUseCase(client client.Client, mgrSet *util.ManagerSet
 			upgrade.NewDeleteExecPodStep(mgrSet),
 			upgrade.NewRevertStartupProbeStep(client, mgrSet, namespace),
 			upgrade.NewDeleteDevelopmentDoguMapStep(client, mgrSet),
+			upgrade.NewInstalledVersionStep(mgrSet, namespace),
 			upgrade.NewDeploymentUpdaterStep(client, mgrSet, namespace),
 		},
 	}
