be9ca8adfef67e1ef7fa265483f31d0337148ea1
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-09-25 11:14:08 +0200

    #259 add tests for volumeExpanderStep

--- controllers/steps/postinstall/volumeExpanderStep_test.go
+++ controllers/steps/postinstall/volumeExpanderStep_test.go
@@ -1,13 +1,18 @@
 package postinstall
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cloudogu/ces-commons-lib/dogu"
+	"github.com/cloudogu/cesapp-lib/core"
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestNewVolumeExpanderStep(t *testing.T) {
@@ -54,6 +59,121 @@ func TestVolumeExpanderStep_Run(t *testing.T) {
 			},
 			doguResource: &v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}},
 			want:         steps.RequeueWithError(assert.AnError),
+		},
+		{
+			name: "should fail to update success condition if no volume exists",
+			fields: fields{
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+				doguInterfaceFn: func(t *testing.T) doguInterface {
+					condition := v1.Condition{
+						Type:               v2.ConditionMeetsMinVolumeSize,
+						Status:             v1.ConditionTrue,
+						Reason:             ActualVolumeSizeMeetsMinDataSize,
+						Message:            "Current VolumeSize meets the configured minimum VolumeSize",
+						LastTransitionTime: v1.Now().Rfc3339Copy(),
+					}
+					d := &v2.Dogu{
+						ObjectMeta: v1.ObjectMeta{Name: "test"},
+						Status: v2.DoguStatus{
+							Conditions: []v1.Condition{condition},
+						},
+					}
+					mck := newMockDoguInterface(t)
+					mck.EXPECT().UpdateStatus(testCtx, d, v1.UpdateOptions{}).Return(nil, assert.AnError)
+					return mck
+				},
+				localDoguFetcherFn: func(t *testing.T) localDoguFetcher {
+					mck := newMockLocalDoguFetcher(t)
+					mck.EXPECT().FetchInstalled(testCtx, dogu.SimpleName("test")).Return(&core.Dogu{}, nil)
+					return mck
+				},
+			},
+			doguResource: &v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}},
+			want:         steps.RequeueWithError(assert.AnError),
+		},
+		{
+			name: "should reconcile if volume is expanded",
+			fields: fields{
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+				doguInterfaceFn: func(t *testing.T) doguInterface {
+					condition := v1.Condition{
+						Type:               v2.ConditionMeetsMinVolumeSize,
+						Status:             v1.ConditionTrue,
+						Reason:             ActualVolumeSizeMeetsMinDataSize,
+						Message:            "Current VolumeSize meets the configured minimum VolumeSize",
+						LastTransitionTime: v1.Now().Rfc3339Copy(),
+					}
+					d := &v2.Dogu{
+						ObjectMeta: v1.ObjectMeta{Name: "test"},
+						Status: v2.DoguStatus{
+							Conditions: []v1.Condition{condition},
+						},
+					}
+					mck := newMockDoguInterface(t)
+					mck.EXPECT().UpdateStatus(testCtx, d, v1.UpdateOptions{}).Return(d, nil)
+					return mck
+				},
+				localDoguFetcherFn: func(t *testing.T) localDoguFetcher {
+					mck := newMockLocalDoguFetcher(t)
+					mck.EXPECT().FetchInstalled(testCtx, dogu.SimpleName("test")).Return(&core.Dogu{}, nil)
+					return mck
+				},
+			},
+			doguResource: &v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}},
+			want:         steps.Continue(),
+		},
+		{
+			name: "should fail to get data pvc",
+			fields: fields{
+				clientFn: func(t *testing.T) k8sClient {
+					mck := newMockK8sClient(t)
+					mck.EXPECT().Get(testCtx, types.NamespacedName{Name: "test"}, &corev1.PersistentVolumeClaim{}).Return(assert.AnError)
+					return mck
+				},
+				doguInterfaceFn: func(t *testing.T) doguInterface {
+					return newMockDoguInterface(t)
+				},
+				localDoguFetcherFn: func(t *testing.T) localDoguFetcher {
+					mck := newMockLocalDoguFetcher(t)
+					mck.EXPECT().FetchInstalled(testCtx, dogu.SimpleName("test")).Return(&core.Dogu{
+						Name:    "test",
+						Volumes: []core.Volume{{Name: "test", NeedsBackup: true}},
+					}, nil)
+					return mck
+				},
+			},
+			doguResource: &v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}},
+			want:         steps.RequeueWithError(fmt.Errorf("failed to get data pvc for dogu test: %w", assert.AnError)),
+		},
+		{
+			name: "should fail to set success condition if resized",
+			fields: fields{
+				clientFn: func(t *testing.T) k8sClient {
+					mck := newMockK8sClient(t)
+					mck.EXPECT().Get(testCtx, types.NamespacedName{Name: "test"}, &corev1.PersistentVolumeClaim{}).Return(nil)
+					mck.EXPECT().Update(testCtx, mock.Anything).Return(nil)
+					return mck
+				},
+				doguInterfaceFn: func(t *testing.T) doguInterface {
+					mck := newMockDoguInterface(t)
+					mck.EXPECT().UpdateStatus(testCtx, mock.Anything, v1.UpdateOptions{}).Return(&v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}}, nil)
+					return mck
+				},
+				localDoguFetcherFn: func(t *testing.T) localDoguFetcher {
+					mck := newMockLocalDoguFetcher(t)
+					mck.EXPECT().FetchInstalled(testCtx, dogu.SimpleName("test")).Return(&core.Dogu{
+						Name:    "test",
+						Volumes: []core.Volume{{Name: "test", NeedsBackup: true}},
+					}, nil)
+					return mck
+				},
+			},
+			doguResource: &v2.Dogu{ObjectMeta: v1.ObjectMeta{Name: "test"}},
+			want:         steps.RequeueAfter(requeueAfterVolume),
 		},
 	}
 	for _, tt := range tests {
