f9fc9b97eb5e9d295c46afd99be0ba84eac84e05
Niklas Roeske <niklas.roeske@cloudogu.com>
2025-10-01 10:31:37 +0200

    #259 add tests for fetchRemoteDoguDescriptorStep

--- controllers/steps/install/fetchRemoteDoguDescriptorStep_test.go
+++ controllers/steps/install/fetchRemoteDoguDescriptorStep_test.go
@@ -1,12 +1,17 @@
 package install
 
 import (
-	"context"
+	"fmt"
 	"testing"
 
+	"github.com/cloudogu/ces-commons-lib/dogu"
+	"github.com/cloudogu/ces-commons-lib/errors"
+	"github.com/cloudogu/cesapp-lib/core"
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps"
 	"github.com/stretchr/testify/assert"
+	v3 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestNewFetchRemoteDoguDescriptorStep(t *testing.T) {
@@ -15,32 +20,186 @@ func TestNewFetchRemoteDoguDescriptorStep(t *testing.T) {
 }
 
 func TestFetchRemoteDoguDescriptorStep_Run(t *testing.T) {
-	t.Fatal("not implemented")
 	type fields struct {
-		client                  k8sClient
-		resourceDoguFetcher     resourceDoguFetcher
-		localDoguDescriptorRepo localDoguDescriptorRepository
-	}
-	type args struct {
-		ctx      context.Context
-		resource *v2.Dogu
+		clientFn                  func(t *testing.T) k8sClient
+		resourceDoguFetcherFn     func(t *testing.T) resourceDoguFetcher
+		localDoguDescriptorRepoFn func(t *testing.T) localDoguDescriptorRepository
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   steps.StepResult
+		name     string
+		fields   fields
+		resource *v2.Dogu
+		want     steps.StepResult
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should fail to get SimpleNameVersion",
+			fields: fields{
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					return newMockResourceDoguFetcher(t)
+				},
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					return newMockLocalDoguDescriptorRepository(t)
+				},
+			},
+			resource: &v2.Dogu{
+				Spec: v2.DoguSpec{
+					Version: "---",
+				},
+			},
+			want: steps.RequeueWithError(fmt.Errorf("found more than one hyphen in version ---")),
+		},
+		{
+			name: "should fail to get local dogu descriptor",
+			fields: fields{
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					mck := newMockLocalDoguDescriptorRepository(t)
+					mck.EXPECT().Get(testCtx, dogu.SimpleNameVersion{Name: "test", Version: core.Version{Raw: "1.0.0", Major: 1}}).Return(nil, assert.AnError)
+					return mck
+				},
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					return newMockResourceDoguFetcher(t)
+				},
+			},
+			resource: &v2.Dogu{
+				ObjectMeta: v1.ObjectMeta{Name: "test"},
+				Spec: v2.DoguSpec{
+					Version: "1.0.0",
+				},
+			},
+			want: steps.RequeueWithError(assert.AnError),
+		},
+		{
+			name: "should continue if local dogu descriptor is found",
+			fields: fields{
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					mck := newMockLocalDoguDescriptorRepository(t)
+					mck.EXPECT().Get(testCtx, dogu.SimpleNameVersion{Name: "test", Version: core.Version{Raw: "1.0.0", Major: 1}}).Return(&core.Dogu{}, nil)
+					return mck
+				},
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					return newMockResourceDoguFetcher(t)
+				},
+			},
+			resource: &v2.Dogu{
+				ObjectMeta: v1.ObjectMeta{Name: "test"},
+				Spec: v2.DoguSpec{
+					Version: "1.0.0",
+				},
+			},
+			want: steps.Continue(),
+		},
+		{
+			name: "should fail to fetch remote dogu descriptor",
+			fields: fields{
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					mck := newMockLocalDoguDescriptorRepository(t)
+					mck.EXPECT().Get(testCtx, dogu.SimpleNameVersion{Name: "test", Version: core.Version{Raw: "1.0.0", Major: 1}}).Return(nil, errors.NewNotFoundError(assert.AnError))
+					return mck
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					mck := newMockResourceDoguFetcher(t)
+					mck.EXPECT().FetchWithResource(testCtx, &v2.Dogu{
+						ObjectMeta: v1.ObjectMeta{Name: "test"},
+						Spec: v2.DoguSpec{
+							Version: "1.0.0",
+						},
+					}).Return(nil, nil, assert.AnError)
+					return mck
+				},
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+			},
+			resource: &v2.Dogu{
+				ObjectMeta: v1.ObjectMeta{Name: "test"},
+				Spec: v2.DoguSpec{
+					Version: "1.0.0",
+				},
+			},
+			want: steps.RequeueWithError(assert.AnError),
+		},
+		{
+			name: "should fail to add remote dogu descriptor to local dogu descriptor repo",
+			fields: fields{
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					mck := newMockLocalDoguDescriptorRepository(t)
+					mck.EXPECT().Get(testCtx, dogu.SimpleNameVersion{Name: "test", Version: core.Version{Raw: "1.0.0", Major: 1}}).Return(nil, errors.NewNotFoundError(assert.AnError))
+					mck.EXPECT().Add(testCtx, dogu.SimpleName("test"), &core.Dogu{Name: "test", Version: "1.0.0"}).Return(assert.AnError)
+					return mck
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					mck := newMockResourceDoguFetcher(t)
+					mck.EXPECT().FetchWithResource(testCtx, &v2.Dogu{
+						ObjectMeta: v1.ObjectMeta{Name: "test"},
+						Spec: v2.DoguSpec{
+							Version: "1.0.0",
+						},
+					}).Return(&core.Dogu{Name: "test", Version: "1.0.0"}, nil, nil)
+					return mck
+				},
+				clientFn: func(t *testing.T) k8sClient {
+					return newMockK8sClient(t)
+				},
+			},
+			resource: &v2.Dogu{
+				ObjectMeta: v1.ObjectMeta{Name: "test"},
+				Spec: v2.DoguSpec{
+					Version: "1.0.0",
+				},
+			},
+			want: steps.RequeueWithError(assert.AnError),
+		},
+		{
+			name: "should fail to delete development dogu map from cluster",
+			fields: fields{
+				localDoguDescriptorRepoFn: func(t *testing.T) localDoguDescriptorRepository {
+					mck := newMockLocalDoguDescriptorRepository(t)
+					mck.EXPECT().Get(testCtx, dogu.SimpleNameVersion{Name: "test", Version: core.Version{Raw: "1.0.0", Major: 1}}).Return(nil, errors.NewNotFoundError(assert.AnError))
+					mck.EXPECT().Add(testCtx, dogu.SimpleName("test"), &core.Dogu{Name: "test", Version: "1.0.0"}).Return(nil)
+					return mck
+				},
+				resourceDoguFetcherFn: func(t *testing.T) resourceDoguFetcher {
+					mck := newMockResourceDoguFetcher(t)
+					mck.EXPECT().FetchWithResource(testCtx, &v2.Dogu{
+						ObjectMeta: v1.ObjectMeta{Name: "test"},
+						Spec: v2.DoguSpec{
+							Version: "1.0.0",
+						},
+					}).Return(&core.Dogu{Name: "test", Version: "1.0.0"}, &v2.DevelopmentDoguMap{}, nil)
+					return mck
+				},
+				clientFn: func(t *testing.T) k8sClient {
+					mck := newMockK8sClient(t)
+					mck.EXPECT().Delete(testCtx, &v3.ConfigMap{}).Return(assert.AnError)
+					return mck
+				},
+			},
+			resource: &v2.Dogu{
+				ObjectMeta: v1.ObjectMeta{Name: "test"},
+				Spec: v2.DoguSpec{
+					Version: "1.0.0",
+				},
+			},
+			want: steps.Continue(),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := &FetchRemoteDoguDescriptorStep{
-				client:                  tt.fields.client,
-				resourceDoguFetcher:     tt.fields.resourceDoguFetcher,
-				localDoguDescriptorRepo: tt.fields.localDoguDescriptorRepo,
+				client:                  tt.fields.clientFn(t),
+				resourceDoguFetcher:     tt.fields.resourceDoguFetcherFn(t),
+				localDoguDescriptorRepo: tt.fields.localDoguDescriptorRepoFn(t),
 			}
-			assert.Equalf(t, tt.want, f.Run(tt.args.ctx, tt.args.resource), "Run(%v, %v)", tt.args.ctx, tt.args.resource)
+			assert.Equalf(t, tt.want, f.Run(testCtx, tt.resource), "Run(%v, %v)", testCtx, tt.resource)
 		})
 	}
 }
