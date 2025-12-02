d76c5b86455675aec3f9c23e90d5aeabf6f72bde
Laurin Friebe <laurin.friebe-extern@cloudogu.com>
2025-12-02 11:25:45 +0100

    add removeServiceAccount step to usecase

--- controllers/steps/install/removeServiceAccountStep.go
+++ controllers/steps/install/removeServiceAccountStep.go
@@ -4,26 +4,31 @@ import (
 	"context"
 
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
+	doguClient "github.com/cloudogu/k8s-dogu-lib/v2/client"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/cesregistry"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/serviceaccount"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // The RemoveServiceAccountStep removes service accounts to components that the dogu might have
+// these service accounts need to be recreated if the dogu was restored from a backup
 type RemoveServiceAccountStep struct {
 	serviceAccountRemover serviceAccountRemover
 	localDoguFetcher      localDoguFetcher
+	doguInterface         doguInterface
 }
 
-func NewRemoveServiceAccountStep(creator serviceaccount.ServiceAccountRemover, fetcher cesregistry.LocalDoguFetcher) *RemoveServiceAccountStep {
+func NewRemoveServiceAccountStep(creator serviceaccount.ServiceAccountRemover, fetcher cesregistry.LocalDoguFetcher, doguInterface doguClient.DoguInterface) *RemoveServiceAccountStep {
 	return &RemoveServiceAccountStep{
 		serviceAccountRemover: creator,
 		localDoguFetcher:      fetcher,
+		doguInterface:         doguInterface,
 	}
 }
 
 func (sas *RemoveServiceAccountStep) Run(ctx context.Context, doguResource *v2.Dogu) steps.StepResult {
-	const restoredAnnotation = "k8s.cloudogu.com/was-restored"
+	const restoredAnnotation = "wasRestored"
 	doguDescriptor, err := sas.localDoguFetcher.FetchInstalled(ctx, doguResource.GetSimpleDoguName())
 	if err != nil {
 		return steps.RequeueWithError(err)
@@ -32,6 +37,9 @@ func (sas *RemoveServiceAccountStep) Run(ctx context.Context, doguResource *v2.D
 	if doguResource.Annotations != nil {
 		if _, wasRestored := doguResource.Annotations[restoredAnnotation]; wasRestored {
 			err = sas.serviceAccountRemover.RemoveAllFromComponents(ctx, doguDescriptor)
+			// remove annotation afterward to prevent endless requeue
+			delete(doguResource.Annotations, restoredAnnotation)
+			_, err = sas.doguInterface.Update(ctx, doguResource, metav1.UpdateOptions{})
 		}
 	}
 
