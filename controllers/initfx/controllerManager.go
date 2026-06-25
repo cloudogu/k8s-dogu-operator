79a2c14473b6d37261b8b813dcc29123c342588e
Henry Bobka <henry.bobka@cloudogu.com>
2026-06-25 17:02:37 +0200

    313: wire warpMenuEntryStep into install usecase and operator setup

--- controllers/initfx/controllerManager.go
+++ controllers/initfx/controllerManager.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/config"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/health"
 	expositionv1 "github.com/cloudogu/k8s-exposition-lib/api/v1"
+	warpmenuentryv1 "github.com/cloudogu/k8s-warp-menu-entry-lib/api/v1"
 
 	"github.com/go-logr/logr"
 	"github.com/google/uuid"
@@ -42,6 +43,7 @@ func init() {
 	utilruntime.Must(doguv2.AddToScheme(scheme))
 	utilruntime.Must(authRegApiV1.AddToScheme(scheme))
 	utilruntime.Must(expositionv1.AddToScheme(scheme))
+	utilruntime.Must(warpmenuentryv1.AddToScheme(scheme))
 }
 
 func NewControllerManager(
