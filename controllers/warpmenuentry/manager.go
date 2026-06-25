a343400155f3126261a4cddd4f86219a8f3d45c2
Peter Mörchen <peter.moerchen@cloudogu.com>
2026-06-25 09:59:26 +0200

    313: Fix failing test and add new for missing dogu

--- controllers/warpmenuentry/manager.go
+++ controllers/warpmenuentry/manager.go
@@ -2,6 +2,7 @@ package warpmenuentry
 
 import (
 	"context"
+	"errors"
 
 	"github.com/cloudogu/ces-commons-lib/dogu"
 	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
@@ -15,8 +16,10 @@ func NewWarpMenuEntryManager() *WarpMenuEntryManager {
 }
 
 func (w WarpMenuEntryManager) EnsureWarpMenuEntry(ctx context.Context, doguResource *v2.Dogu) error {
-	//TODO implement me
-	panic("implement me")
+	if doguResource == nil {
+		return errors.New("doguResource must not be nil")
+	}
+	return nil
 }
 
 func (w WarpMenuEntryManager) DeleteWarpMenuEntry(ctx context.Context, doguName dogu.SimpleName) error {
--- controllers/warpmenuentry/manager_test.go
+++ controllers/warpmenuentry/manager_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	v2 "github.com/cloudogu/k8s-dogu-lib/v2/api/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -23,6 +24,22 @@ func TestEnsureWarpMenuEntry(t *testing.T) {
 		manager := NewWarpMenuEntryManager()
 		err := manager.EnsureWarpMenuEntry(ctx, nil)
 		require.Error(t, err)
-		assert.ErrorContains(t, err, "doguresource must not be nil")
+		assert.ErrorContains(t, err, "doguResource must not be nil")
 	})
+
+	t.Run("Should return error when dogu spec cannot be retrieved", func(t *testing.T) {
+		manager := NewWarpMenuEntryManager()
+		err := manager.EnsureWarpMenuEntry(ctx, newDoguResource("unknown"))
+		require.Error(t, err)
+		assert.ErrorContains(t, err, "dogu spec cannot be retrieved")
+	})
+
+}
+
+func newDoguResource(doguName string) *v2.Dogu {
+	return &v2.Dogu{
+		Spec: v2.DoguSpec{
+			Name: doguName,
+		},
+	}
 }
