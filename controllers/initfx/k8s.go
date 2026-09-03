f9904a2b2b8ecb85ad2b7af650252c1c0d3f8aef
Philipp Pixel <boris.schaa@cloudogu.com>
2026-09-03 10:35:02 +0200

    #329 Extract dogu version specific dependency injection constructors
    This way, dogu v2 _might_ be easier to get rid of
    
    Co-authored-by: Niklas <niklas.hinze@cloudogu.com>

--- controllers/initfx/k8s.go
+++ controllers/initfx/k8s.go
@@ -1,12 +1,8 @@
 package initfx
 
 import (
-	authRegClientV1 "github.com/cloudogu/k8s-auth-registration-lib/client/typed/api/v1"
 	doguClient "github.com/cloudogu/k8s-dogu-lib/v3/client"
-	doguv2 "github.com/cloudogu/k8s-dogu-lib/v3/client/typed/api/v2"
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/config"
-	expClientV1 "github.com/cloudogu/k8s-exposition-lib/client/typed/api/v1"
-	warpClientV1 "github.com/cloudogu/k8s-warp-menu-entry-lib/client/typed/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	appsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
@@ -29,48 +25,8 @@ func newDoguClientset(config *rest.Config) (doguClient.Interface, error) {
 	return doguClient.NewForConfig(config)
 }
 
-var NewAuthRegistrationClientSet = newAuthRegistrationClientSet
-
-func newAuthRegistrationClientSet(config *rest.Config) (authRegClientV1.ApiV1Interface, error) {
-	return authRegClientV1.NewForConfig(config)
-}
-
-var NewExpositionClientSet = newExpositionClientSet
-
-func newExpositionClientSet(config *rest.Config) (expClientV1.ApiV1Interface, error) {
-	return expClientV1.NewForConfig(config)
-}
-
-var NewWarpMenuEntryClientSet = newWarpMenuEntryClientSet
-
-func newWarpMenuEntryClientSet(config *rest.Config) (warpClientV1.ApiV1Interface, error) {
-	return warpClientV1.NewForConfig(config)
-}
-
 func NewK8sClient(mgr manager.Manager) client.Client {
 	return mgr.GetClient()
-}
-
-func NewDoguInterface(doguClientset doguClient.Interface, config *config.OperatorConfig) doguv2.DoguInterface {
-	return doguClientset.DoguV2().Dogus(config.Namespace)
-}
-
-// TODO create similar constructor for v3
-
-func NewDoguRestartInterface(doguClientset doguClient.Interface, config *config.OperatorConfig) doguv2.DoguRestartInterface {
-	return doguClientset.DoguV2().DoguRestarts(config.Namespace)
-}
-
-func NewAuthRegistrationInterface(authRegistrationClientSet authRegClientV1.ApiV1Interface, operatorConfig *config.OperatorConfig) authRegClientV1.AuthRegistrationInterface {
-	return authRegistrationClientSet.AuthRegistrations(operatorConfig.Namespace)
-}
-
-func NewExpositionInterface(expositionClientSet expClientV1.ApiV1Interface, operatorConfig *config.OperatorConfig) expClientV1.ExpositionInterface {
-	return expositionClientSet.Expositions(operatorConfig.Namespace)
-}
-
-func NewWarpMenuEntryInterface(warpMenuEntryClientSet warpClientV1.ApiV1Interface, operatorConfig *config.OperatorConfig) warpClientV1.WarpMenuEntryInterface {
-	return warpMenuEntryClientSet.WarpMenuEntries(operatorConfig.Namespace)
 }
 
 func NewConfigMapInterface(clientSet kubernetes.Interface, operatorConfig *config.OperatorConfig) v1.ConfigMapInterface {
