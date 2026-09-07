11f76f3836c78385835a2ee5c14da7171f39c82d
Niklas <niklas.hinze@cloudogu.com>
2026-09-07 16:30:27 +0200

    #329 Formating

--- controllers/steps/doguv3/step.go
+++ controllers/steps/doguv3/step.go
@@ -7,9 +7,10 @@ import (
 	"github.com/cloudogu/k8s-dogu-lib/v3/api/v3beta1"
 )
 
-// Step defines a single aspect in a dogu lifecycle phase which usually ends up in changed side-effects. 
+// Step defines a single aspect in a dogu lifecycle phase which usually ends up in changed side-effects.
 //
-// The sequence of which step is ran by the reconciler is fixed. Anyhow, they are supposed to work in an idempotent fashion, so that the step exits early without error if the desired side effect was already accomplished or is going to re-run upon an early error.
+// The sequence of which step is ran by the reconciler is fixed. Anyhow, they are supposed to work in an idempotent fashion,
+// so that the step exits early without error if the desired side effect was already accomplished or is going to re-run upon an early error.
 //
 // Any means for cluster or any other service access must be provided during the step creation.
 type Step interface {
@@ -17,18 +18,15 @@ type Step interface {
 	Run(ctx context.Context, resource *v3beta1.Dogu) StepResult
 }
 
-type StepResult struct {
-	RequeueAfter time.Duration
-	Err          error
-	Continue     bool
 // StepResult indicates if and when a follow-up requeue is in order to continue the reconcilication.
 type StepResult struct {
 	// RequeueAfter contains the earliest duration when the Kubernetes API restarts a new reconciliation loop for the current dogu.
 	RequeueAfter time.Duration
 	// Erro contains an error if one occurred.
-	Err          error
+	Err error
 	// Continue with a value of true flags if another requeue should be conducted. Otherwise, the dogu wil no longer be reconciled. This field will be evaluated after checks for RequeueAfter or Err.
-	Continue     bool
+	Continue bool
+}
 
 // RequeueAfter is a convenience function returning a StepResult with the given duration, indicating a requeue without further error.
 func RequeueAfter(requeueAfter time.Duration) StepResult {
@@ -38,7 +36,7 @@ func RequeueAfter(requeueAfter time.Duration) StepResult {
 }
 
 // Continue is a convenience function returning a StepResult that indicates to positively continue with the next step of the dogu phase.
-// 
+//
 // Its counterpart is Abort() for stopping the dogu's reconciliation.
 func Continue() StepResult {
 	return StepResult{
@@ -47,7 +45,7 @@ func Continue() StepResult {
 }
 
 // Abort is a convenience function returning a StepResult that indicates to end this step and the reconciliation in the dogu's lifecycle in general.
-// 
+//
 // Its counterpart is Continue() for continuing the dogu's reconciliation.
 func Abort() StepResult {
 	return StepResult{
--- controllers/usecase/doguv3/usecase.go
+++ controllers/usecase/doguv3/usecase.go
@@ -9,7 +9,9 @@ import (
 	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/steps/doguv3/install"
 )
 
-// DoguUseCase is a controlling structure responsible for handling all those actions that occur within the dogu's lifecycle phase. The actions are set up as [v3.Step]s during the use-case construction, independently of the dogu at hand, i. e. different dogus within the same lifecycle phase traverse the same list of steps.
+// DoguUseCase is a controlling structure responsible for handling all those actions that occur within the dogu's lifecycle phase.
+// The actions are set up as [v3.Step]s during the use-case construction, independently of the dogu at hand, i. e. different
+// dogus within the same lifecycle phase traverse the same list of steps.
 type DoguUseCase struct {
 	steps []v3.Step
 }
