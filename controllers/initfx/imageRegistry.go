f03443718ad5ada17807e28e37cf8cf771d77fc5
Nico Franzeck <nico.franzeck@cloudogu.com>
2026-09-10 20:21:07 +0200

    [#332] Implement image config cache

--- controllers/initfx/imageRegistry.go
+++ controllers/initfx/imageRegistry.go
@@ -1,5 +1,11 @@
 package initfx
 
-import "github.com/cloudogu/k8s-dogu-operator/v3/controllers/imageregistry"
+import (
+	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/config"
+	"github.com/cloudogu/k8s-dogu-operator/v3/controllers/imageregistry"
+)
 
-var NewImageRegistry = imageregistry.NewCraneContainerImageRegistry
+// NewImageRegistry creates the container image registry, sizing its image config cache from the operator config.
+func NewImageRegistry(operatorConfig *config.OperatorConfig) imageregistry.ImageRegistry {
+	return imageregistry.NewCraneContainerImageRegistry(operatorConfig.ImageConfigCacheSize)
+}
